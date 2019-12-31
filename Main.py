c7aa8c1e7dd6988b8a89213d5cf4c1ad3abf8bbc
saarzeev <saarzeev@post.bgu.ac.il>
2019-12-31 18:17:32 +0200

    summarizing in progress...

--- Main.py
+++ Main.py
@@ -6,6 +6,7 @@ import os
 import nltk
 nltk.download('wordnet')
 import gensim
+from gensim import corpora, models
 from gensim.utils import simple_preprocess
 from gensim.parsing.preprocessing import STOPWORDS
 from nltk.stem import WordNetLemmatizer, SnowballStemmer
@@ -23,24 +24,26 @@ class DataBase:
     def getSimillar(self,bussinessName,categoriesSim,locationSim,hoursSim):
         print(self.business_Json[self.business_Json['name']==bussinessName].compute())
 
-    def preprocess(self, text):
+    def preprocess_reviews_text(self, text):
         result = []
         for token in gensim.utils.simple_preprocess(text):
             if token not in gensim.parsing.preprocessing.STOPWORDS and len(token) > 2:
                 result.append(WordNetLemmatizer().lemmatize(token, pos='v'))
         return result
 
-    def getShitDone(self, business_id):
-        # business_reviews = self.review_Json[self.review_Json['business_id'] == business_id].compute() # This gets only the business relevant reviews
-        doc_sample = self.review_Json[self.review_Json['business_id'] == business_id]['text']
+    def summarize_reviews(self, business_id):
+        self.review_Json['processed review'] = self.review_Json['text'].map(self.preprocess_reviews_text)
+        dictionary = corpora.Dictionary(self.review_Json['processed review'].compute())
+        dictionary.filter_extremes(no_below=15, no_above=0.75, keep_n=100000)
+        bow_corpus = [dictionary.doc2bow(review) for review in self.review_Json['processed review'].compute()]
 
-        print('original document: ')
-        words = []
-        for word in doc_sample.split(' '):
-            words.append(word)
-        print(words)
-        print('\n\n tokenized and lemmatized document: ')
-        print(self.preprocess(doc_sample))
+
+        tfidf = models.TfidfModel(bow_corpus)
+        lda_model_tfidf = models.LdaMulticore(tfidf[bow_corpus], num_topics=10, id2word=dictionary, passes=2,
+                                                     workers=4)
+
+        for idx, topic in lda_model_tfidf.print_topics(-1):
+            print('Topic: {} Word: {}'.format(idx, topic))
 
 
 
@@ -50,4 +53,4 @@ if __name__ == '__main__':
     cwd = os.getcwd()
     test = DataBase(cwd + "/miniCorpus/")
     #test.getSimillar("Arizona Biltmore Golf Club",True,True,True)
-    test.getShitDone("Gyrez6K8f1AyR7dzW9fvAw")
+    test.summarize_reviews("Gyrez6K8f1AyR7dzW9fvAw")
