9ae51a43dbed3ea2682fed069796df4d3d549d75
ilayFriedman <ilayfri@bgu.ac.il>
2019-12-31 14:55:07 +0200

    firstBuildAPI

--- mainDataSet.py
+++ mainDataSet.py
@@ -3,15 +3,20 @@ import dask.bag as db
 import dask.dataframe as dd
 import json
 
+class DataBase:
 
-def getDetailsOnMe(businessName):
-    review_Json = db.read_text("C:\\Users\\User\\Desktop\\DATA_SET\\review.json").map(json.loads).to_dataframe()
-    business_Json = db.read_text("C:\\Users\\User\\Desktop\\DATA_SET\\business.json").map(json.loads).to_dataframe()
-    checkin_Json = db.read_text("C:\\Users\\User\\Desktop\\DATA_SET\\checkin.json").map(json.loads).to_dataframe()
-    photo_Json = db.read_text("C:\\Users\\User\\Desktop\\DATA_SET\\photo.json").map(json.loads).to_dataframe()
-    tip_Json = db.read_text("C:\\Users\\User\\Desktop\\DATA_SET\\tip.json").map(json.loads).to_dataframe()
-    user_Json = db.read_text("C:\\Users\\User\\Desktop\\DATA_SET\\user.json").map(json.loads).to_dataframe()
-    print(tip_Json.columns)
+    def __init__(self,pathToDataSet):
+        self.review_Json = db.read_text("C:\\Users\\User\\Desktop\\DATA_SET\\review.json").map(json.loads).to_dataframe()
+        self.business_Json = db.read_text("C:\\Users\\User\\Desktop\\DATA_SET\\business.json").map(json.loads).to_dataframe()
+        self.checkin_Json = db.read_text(pathToDataSet+"\\checkin.json").map(json.loads).to_dataframe()
+        self.photo_Json = db.read_text(pathToDataSet+"\\photo.json").map(json.loads).to_dataframe()
+        self.tip_Json = db.read_text(pathToDataSet+"\\tip.json").map(json.loads).to_dataframe()
+        self.user_Json = db.read_text(pathToDataSet+"\\user.json").map(json.loads).to_dataframe()
+
+    def getSimillar(self,bussinessName,categoriesSim,locationSim,hoursSim):
+        print(self.business_Json[self.business_Json['name']==bussinessName].compute())
+
 
 if __name__ == '__main__':
-    getDetailsOnMe("efds")
+    test = DataBase("C:\\Users\\User\\Desktop\\DATA_SET")
+    test.getSimillar("Arizona Biltmore Golf Club",True,True,True)
